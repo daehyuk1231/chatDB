98f56249a8a0cd994cdf2e52f89ff91d9e773d67
daehyuk1231 <bangdaehyeog@bangdaehyeog-ui-MacBookAir.local>
2025-01-08 12:23:51 +0900

    Data 3 add

--- src/main/java/com/ll/chatDB/global/initData/NotProd.java
+++ src/main/java/com/ll/chatDB/global/initData/NotProd.java
@@ -3,6 +3,8 @@ package com.ll.chatDB.global.initData;
 import com.ll.chatDB.domain.chat.chatMessage.service.ChatMessageService;
 import com.ll.chatDB.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chatDB.domain.chat.chatRoom.service.ChatRoomService;
+import com.ll.chatDB.domain.chat.member.member.entity.Member;
+import com.ll.chatDB.domain.chat.member.member.service.MemberService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +16,7 @@ import java.util.stream.IntStream;
 @Profile("!prod")
 public class NotProd {
     @Bean
-    public ApplicationRunner applicationRunner(ChatRoomService chatRoomService, ChatMessageService chatMessageService) {
+    public ApplicationRunner applicationRunner(ChatRoomService chatRoomService, ChatMessageService chatMessageService, MemberService memberService) {
         return args -> {
             ChatRoom chatRoom1 = chatRoomService.create("room1");
             ChatRoom chatRoom2 = chatRoomService.create("room2");
@@ -23,6 +25,9 @@ public class NotProd {
             IntStream.rangeClosed(1, 100).forEach(num -> {
                 chatMessageService.create(chatRoom1, "홍길동", "채팅메세지" + num);
             });
+            Member member1 = memberService.join("user1", "1234").getData();
+            Member member2 = memberService.join("user2", "1234").getData();
+            Member member3 = memberService.join("user3", "1234").getData();
             System.out.println("This is not a production environment.");
         };
     }
