b03ffcbc5a00792d63b678982404dbb83076f096
daehyuk1231 <bangdaehyeog@bangdaehyeog-ui-MacBookAir.local>
2025-01-10 12:30:04 +0900

    Member Signup

---
+++ src/main/java/com/ll/chatDB/domain/member/member/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.ll.chatDB.domain.member.member.dto;
+
+import com.ll.chatDB.domain.member.member.entity.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberDto {
+    private String username;
+    public MemberDto(Member member) {
+        this.username = member.getUsername();
+    }
+}
