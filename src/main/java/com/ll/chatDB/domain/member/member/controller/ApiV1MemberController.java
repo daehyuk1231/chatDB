e7cef892c7eab518bfc63439815655d8fb0e0ce1
daehyuk1231 <bangdaehyeog@bangdaehyeog-ui-MacBookAir.local>
2025-01-10 17:12:29 +0900

    Logout

--- src/main/java/com/ll/chatDB/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/chatDB/domain/member/member/controller/ApiV1MemberController.java
@@ -44,12 +44,31 @@ public class ApiV1MemberController {
             cookie.setMaxAge(60 * 60);
         response.addCookie(cookie);
 
+        String refreshToken = member.getRefreshToken();
+        Cookie refreshTokenCookie  = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(true);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(60 * 60);
+        response.addCookie(refreshTokenCookie);
+
         return new RsData<>("200", "로그인에 성공하였습니다.");
     }
 
     @GetMapping("/logout")
-    public void logout() {
-        System.out.println("logout");
+    public RsData<Void> logout(HttpServletResponse response) {
+        // 응답 데이터에 accessToken 이름으로 토큰을 발급
+        Cookie cookie = new Cookie("accessToken", null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        Cookie refreshTokenCookie  = new Cookie("refreshToken", null);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(0);
+        response.addCookie(refreshTokenCookie);
+
+        return new RsData<>("200", "로그아웃에 성공하였습니다.");
     }
 
     @GetMapping("/me")
