81a146eb0d4490b98e3859cf1c166e534edb3f0a
daehyuk1231 <bangdaehyeog@bangdaehyeog-ui-MacBookAir.local>
2025-01-10 14:52:52 +0900

    JWT Provider

--- src/main/java/com/ll/chatDB/global/utill/Ut.java
+++ src/main/java/com/ll/chatDB/global/utill/Ut.java
@@ -1,13 +1,26 @@
 package com.ll.chatDB.global.utill;
 
-import lombok.SneakyThrows;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Ut {
-    public static class thread {
-
-        @SneakyThrows
-        public static void sleep(int milli) {
-            Thread.sleep(milli);
+    public static class json {
+        public static Object toStr(Map<String, Object> map) {
+            try {
+                return new ObjectMapper().writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+    }
+    public static Map<String, Object> toMap(String jsonStr) {
+        try {
+            return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+        } catch (JsonProcessingException e) {
+            return null;
         }
     }
 }
